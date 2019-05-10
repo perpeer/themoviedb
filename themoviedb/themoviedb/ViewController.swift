4ab51aab024058be2afcdd6b8207df37884f628e
perpeer <ekremtaskiran@Ekrems-MacBook-Pro.local>
2019-05-10 08:40:49 +0300

    Project created

---
+++ themoviedb/themoviedb/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  themoviedb
+//
+//  Created by Ekrem TAŞKIRAN on 10.05.2019.
+//  Copyright © 2019 Ekrem TAŞKIRAN. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
